24411cf0db219227a716ce13179130fdf5380eb6
moayed-mustafa <moayed.abrida@yahoo.com>
2020-07-08 21:54:30 +0400

    showing number info on the view

--- NumberFacts.js
+++ NumberFacts.js
@@ -13,6 +13,7 @@ Once you have them all, put them on the page. It’s okay if some of the facts a
 (Note: You’ll need to make multiple requests for this.)
  */
 base_url = 'http://numbersapi.com/'
+let ul = document.getElementById('info')
     let promise_list = []
 const specialNumber = (url, i) => {
         let res = axios.get(`${url}${i}`)
@@ -21,11 +22,6 @@ const specialNumber = (url, i) => {
 
 }
 
-// specialNumber(base_url, 0).then(data => {
-//     console.log(data)
-//     // you would want to call a function that would draw stuff on the UI
-// })
-//     .catch(err => console.log(err))
 
 for (let i = 0; i < 5; i++){
     let p = specialNumber(base_url, i)
@@ -34,5 +30,16 @@ for (let i = 0; i < 5; i++){
 }
 console.log(promise_list)
 Promise.all(promise_list).then(function (values) {
-    console.log(values)
+
+    displayData(values)
+
 })
+
+function displayData(list) {
+    for (i of list) {
+        console.log(i.data)
+        let li = document.createElement('li')
+        li.innerHTML = i.data
+        ul.append(li)
+    }
+}
