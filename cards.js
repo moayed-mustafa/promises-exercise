aa56a5264dcfd3ac07941ba679d3e95bceb9f7ae
moayed-mustafa <moayed.abrida@yahoo.com>
2020-07-09 15:08:24 +0400

    fixed the way Im fetching two cards at once

--- cards.js
+++ cards.js
@@ -16,44 +16,46 @@ and show a button on the page that will let you draw a card. Every time you clic
 
 const deck_url = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1'
 let cards = []
+let cardImg= document.createElement('img')
+let drawCards = document.querySelector('#draw')
+console.log(drawCards)
+// drawCards.addEventListener('click', function () {
+//     console.log('hi')
+// })
 const shuffleDeck = (url) => {
      return axios.get(url)
 }
 // * Make a request to the Deck of Cards API to request a single card from a newly shuffled deck.
 //  *  Once you have the card, console.log the value and the suit (e.g. “5 of spades”, “queen of diamonds”). ✔︎
- let card = shuffleDeck(deck_url).then(data => {
-    console.log(data.data.deck_id)
+let card = shuffleDeck(deck_url).then(data => {
     return axios.get(`https://deckofcardsapi.com/api/deck/${data.data.deck_id}/draw/?count=1
     `)
  })
      .catch(err => console.log(err))
 
-card.then(data => {
+drawCard = card.then(data => {
     let card_details = data.data.cards[0]
-    console.log(data.config.url)
-         console.log(card_details.suit, card_details.value)
+    createImage(data.data.cards[0].image)
+
      })
 
+const createImage = (href) => {
+    // let cardImg= document.createElement('img')
+    cardImg.setAttribute('src', href)
+    div = document.querySelector('.imgs')
+    div.append(cardImg)
+}
 //  Make a request to the deck of cards API to request a single card from a newly shuffled deck.
 //  Once you have the card, make a request to the same API to get one more card from the same deck. ✔︎
 let twoCard = shuffleDeck(deck_url).then(data => {
 
-    console.log(data.data.deck_id)
-    axios.get(`https://deckofcardsapi.com/api/deck/${data.data.deck_id}/draw/?count=1
+    axios.get(`https://deckofcardsapi.com/api/deck/${data.data.deck_id}/draw/?count=2
     `).then(data => {
-        cards.push({'suit':data.data.cards[0].suit, 'value':data.data.cards[0].value})
-        axios.get(data.config.url)
-            .then(data => {
-                axios.get(`https://deckofcardsapi.com/api/deck/${data.data.deck_id}/draw/?count=1`)
-                    .then(data => {
-                        cards.push({'suit':data.data.cards[0].suit, 'value':data.data.cards[0].value})
-                    })
+        console.log(data.data.cards)
 
-            })
     })
 }).catch(err => console.log(err))
 
-console.log(cards)
 
 
 // Build an HTML page that lets you draw cards from a deck.
