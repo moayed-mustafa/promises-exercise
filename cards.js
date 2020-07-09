6a7694b86f5f5ae7408cbe00150f8b7601f373fe
moayed-mustafa <moayed.abrida@yahoo.com>
2020-07-09 15:29:17 +0400

    showing cards of a deck dynamically

--- cards.js
+++ cards.js
@@ -17,8 +17,7 @@ and show a button on the page that will let you draw a card. Every time you clic
 const deck_url = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1'
 let cards = []
 let cardImg= document.createElement('img')
-let drawCards = document.querySelector('#draw')
-console.log(drawCards)
+
 // drawCards.addEventListener('click', function () {
 //     console.log('hi')
 // })
@@ -33,20 +32,19 @@ let card = shuffleDeck(deck_url).then(data => {
  })
      .catch(err => console.log(err))
 
-drawCard = card.then(data => {
+card.then(data => {
+    let btn = document.querySelector('.draw')
+    console.log(btn)
     let card_details = data.data.cards[0]
-    createImage(data.data.cards[0].image)
+    console.log(data.data.cards[0].value, data.data.cards[0].suit )
+
 
      })
 
-const createImage = (href) => {
-    // let cardImg= document.createElement('img')
-    cardImg.setAttribute('src', href)
-    div = document.querySelector('.imgs')
-    div.append(cardImg)
-}
+
 //  Make a request to the deck of cards API to request a single card from a newly shuffled deck.
 //  Once you have the card, make a request to the same API to get one more card from the same deck. ✔︎
+//  I changed this to draw cards in one request
 let twoCard = shuffleDeck(deck_url).then(data => {
 
     axios.get(`https://deckofcardsapi.com/api/deck/${data.data.deck_id}/draw/?count=2
@@ -61,3 +59,33 @@ let twoCard = shuffleDeck(deck_url).then(data => {
 // Build an HTML page that lets you draw cards from a deck.
 // When the page loads, go to the Deck of Cards API to create a new deck,
 // and show a button on the page that will let you draw a card. Every time you click the button, display a new card, until there are no cards left in the deck
+
+let allDeck = shuffleDeck(deck_url).then(data => {
+    return axios.get(`https://deckofcardsapi.com/api/deck/${data.data.deck_id}/draw/?count=52
+    `)
+ })
+     .catch(err => console.log(err))
+
+drawCard = allDeck.then(data => {
+    let i = 0;
+    let btn = document.querySelector('.draw')
+    console.log(btn)
+    let card_details = data.data.cards[0]
+    btn.addEventListener('click', function () {
+        console.log(data.data.cards)
+        if(i < data.data.cards.length) {
+            createImage(data.data.cards[i].image)
+        }
+        i++
+        console.log(i)
+    })
+
+})
+
+const createImage = (href) => {
+    // let cardImg= document.createElement('img')
+    let cardImg= document.createElement('img')
+    cardImg.setAttribute('src', href)
+    div = document.querySelector('.imgs')
+    div.append(cardImg)
+}
